801c5768884d2e306006f1705f469e2c8307fb62
mznmk <mika_develop@mznmk.com>
2021-09-05 14:27:44 +0900

    [Problems] solve 01995

---
+++ Problems/01995/main.py
@@ -0,0 +1,29 @@
+#
+# 01995. Count Special Quadruplets
+#
+
+from typing import List
+import itertools
+
+# ============================================================================ #
+
+class Solution:
+    def countQuadruplets(self, nums: List[int]) -> int:
+        count = 0
+        for v in itertools.combinations(nums, 4):
+            if v[0] + v[1] + v[2] == v[3]:
+                count += 1
+        return count
+        
+# ============================================================================ #
+
+cls = Solution()
+
+ans = cls.countQuadruplets([1,2,3,6])
+print(ans == 1)
+
+ans = cls.countQuadruplets([3,3,6,4,5])
+print(ans == 0)
+
+ans = cls.countQuadruplets([1,1,1,3,5])
+print(ans == 4)
